0053babd147b1b6c13511c760c9de19517fdb44f
Erock FaQue <eric.kemerer@gmail.com>
2022-02-08 16:51:22 -0500

    updated api

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ var displayWeather = function(weather, searchCity){
 }
 
 var getUvIndex = function(lat,lon){
-    var apiKey = "844421298d794574c100e3409cee0499"
+    var apiKey = "d71004fdd4ece2a5decfed455e595a25"
     var apiURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`
     fetch(apiURL)
     .then(function(response){
@@ -118,7 +118,7 @@ var displayUvIndex = function(index){
 }
 
 var get5Day = function(city){
-    var apiKey = "844421298d794574c100e3409cee0499"
+    var apiKey = "d71004fdd4ece2a5decfed455e595a25"
     var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
 
     fetch(apiURL)
